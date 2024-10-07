0b9cadae33dd1c91928ffab80964d0ecdda14ee6
aylamir <1116105@apps.nsd.org>
2024-10-07 04:42:12 +0000

    Done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,97 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App{
 
-package org.nhs.app;
+    public static void main (String args[]){
 
-// Your code goes here
+      one();
+      line();
+      two();
+      line();
+      three();
+      line();
+      challenge();
+
+      
+
+    }
+
+    public static void one() {
+        for (int number = 1; number <= 5; number++){
+            for (int counter = 1; counter <= number; counter++){
+               System.out.print(number);
+             }
+             System.out.println();
+         }
+      
+   } 
+
+
+   public static void two() {
+      for (int x = 1; x <= 5; x++){
+         for (int dot = 1; dot <= 5 - x; dot++){
+            System.out.print(".");
+         }
+         for (int num = 1; num <= x; num++){
+            System.out.print(x);
+         }
+         System.out.println();
+      }
+   }
+
+
+   public static void three() {
+      for (int x = 1; x <= 5; x++){
+         for (int dot = 1; dot <= 5 - x; dot++){
+            System.out.print(".");
+         }
+
+         System.out.print(x);
+      
+         for (int dot2 = 1; dot2 <= x - 1; dot2++){
+            System.out.print(".");
+         }
+         System.out.println();
+      }
+   }
+
+
+   public static void challenge() {
+      int width = 9;
+      for (int x = 1; x <= width; x++){
+         for (int space = 1; space <= width - x; space++){
+            System.out.print(" ");
+         }
+         for (int num1 = 1; num1 <=x * 2; num1 ++){
+            System.out.print(x);
+         }
+         System.out.println();
+      }
+      for (int x = width - 1; x >= 1; x--){
+         for (int space = 1; space <= width - x; space++){
+            System.out.print(" ");
+         }
+         for (int num1 = 1; num1 <=x * 2; num1 ++){
+            System.out.print(x);
+         }
+         System.out.println();
+      }
+   }
+
+
+
+   public static void line() {
+      System.out.println();
+   }
+}
+
+
+
+
+    /* public static void main (String[] args){
+       for (int number = 1; number <=5; number ++){
+          for (int counter = 1; counter <=4; counter++){
+             System.out.print(number);
+           }
+           System.out.println();
+       }
+    }
+ }  */
